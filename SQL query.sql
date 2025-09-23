352f8d07fd0d0b7e06246e6b522d1e9fdbaa98a0
Venkatesan C <108179969+imvenkatesanc@users.noreply.github.com>
2025-09-23 19:27:07 +0530

    Update SQL query.sql

--- SQL query.sql
+++ SQL query.sql
@@ -17,3 +17,19 @@ WHERE
     qs.creation_time >= DATEADD(HOUR, -24, GETDATE())
 ORDER BY
     AvgElapsedTime DESC;
+
+
+
+
+
+SELECT TOP 50
+    qsqt.query_sql_text,
+    qsq.execution_type_desc,
+    rs.avg_duration,
+    rs.last_duration,
+    rs.count_executions
+FROM sys.query_store_runtime_stats rs
+JOIN sys.query_store_plan qsp ON rs.plan_id = qsp.plan_id
+JOIN sys.query_store_query qsq ON qsp.query_id = qsq.query_id
+JOIN sys.query_store_query_text qsqt ON qsq.query_text_id = qsqt.query_text_id
+ORDER BY rs.avg_duration DESC;
